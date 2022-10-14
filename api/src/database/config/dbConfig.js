2d449155ff5adc7c2a0b99d3efac7cea74ee128a
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-10-13 22:22:02 -0300

    🧱 : Configurando o database e sequelize

---
+++ api/src/database/config/dbConfig.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: 'localhost',
+    dialect: 'mysql',
+    logging: false,
+    define: {
+      timestamps: true,
+    },
+    timezone: '-03:00'
+  }
+}
